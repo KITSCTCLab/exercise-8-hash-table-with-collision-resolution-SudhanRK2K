32df834286c02565c8264b1dc791a88ed5e36a32
SudhanRK2K <110010355+SudhanRK2K@users.noreply.github.com>
2022-09-20 11:51:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -27,6 +27,7 @@ for item in re.split('], |].', input_data):
   item = item[1:]
   data = item.split(', ')
   if len(data) > 1:
-    insert(HashTable, int(data[0]), data[1])
+    insert(HashTable)
+    insert(int(data[0]), data[1])
 
 display_hash(HashTable)
